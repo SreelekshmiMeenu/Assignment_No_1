837b48290728a7957caf8d87f7ffa4fc0d9e63c9
SreelekshmiMeenu <119158457+SreelekshmiMeenu@users.noreply.github.com>
2023-01-31 12:53:27 +0530

    Add files via upload

---
+++ program_no_2.py
@@ -0,0 +1,6 @@
+a=float(input('Enter the principal amount: '))
+b=float(input('Enter the rate of interest: '))
+c=float(input('Enter the time span: '))
+s= (a*b*c)/100
+print("simple interest is: ", s)
+
